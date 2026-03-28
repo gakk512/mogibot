2fc0cd0662c1dba4a17418f67eeb625f66761d95
gakk512 <example@gmail.com>
2026-03-28 14:12:09 +0900

    add express for render

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server is running");
+});
 const discord = require('discord.js');
 
 const client = new discord.Client({
